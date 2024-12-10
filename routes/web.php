41016ef191df3685e5c56db37d54f04db8714f8e
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-10 18:35:49 +0200

    Explored Route::

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// get, post, put, delete
+
+Route::get('/home', function(){
+    return "this is home page";
+})->name("home");
